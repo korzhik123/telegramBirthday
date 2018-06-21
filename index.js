8081988d8842b7d8512905898fd1633b3d5b24de
Aitbekov Olzhas <>
2018-06-21 14:14:26 -0700

    script for check birthday

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api');
+var schedule = require('node-schedule');
 
 const token = '564178328:AAFuVbWjQc59YwIbe4JQBw0ySgONeSPW7mU';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1];
+const birthday = [
+    {name: 'Мади', bday: new Date('June 4, 1995')},
+    {name: 'Дарика', bday: new Date('June 21, 1996')},
+    {name: 'Мираса', bday: new Date('August 4, 1995')},
+    {name: 'Адиля', bday: new Date('August 21, 1992')},
+    {name: 'Арай', bday: new Date('October 3, 1995')},
+    {name: 'Нурикось', bday: new Date('October 12, 1995')},
+    {name: 'Зарины', bday: new Date('November 4, 1995')},
+    {name: 'Рустема', bday: new Date('November 23, 1995')},
+    {name: 'Елдана', bday: new Date('December 13, 1995')},
+    {name: 'Нолика', bday: new Date('December 16, 1995')},
+    {name: 'Мусы', bday: new Date('February 12, 1996')},
+    {name: 'Коржика', bday: new Date('May 9, 1996')}
+];
 
-  bot.sendMessage(chatId, resp);
+var j = schedule.scheduleJob('20 14 * * *', function(){
+    checkBirthday();
 });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Received your message');
-});
+function checkBirthday(){
+    var datetime = new Date();
+
+    birthday.forEach((person) => {
+        if(person.bday.getMonth() === datetime.getMonth() && person.bday.getDate() === datetime.getDate()) {
+            bot.sendMessage(369098323, `День рождения у ${person.name}, исполнилось ${datetime.getFullYear() - person.bday.getFullYear()}!!!`);
+        }
+    });
+}
